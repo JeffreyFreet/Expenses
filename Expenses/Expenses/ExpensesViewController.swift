a9e21160209c340fc91f4eba07f815b6e3df4f78
Jeffrey Freet <jpfvx8@Jeffreys-MacBook-Pro.local>
2018-04-27 17:57:33 -0500

    Solution to Core Data Challenge

--- Expenses/Expenses/ExpensesViewController.swift
+++ Expenses/Expenses/ExpensesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ExpensesViewController: UIViewController {
     
@@ -14,33 +15,94 @@ class ExpensesViewController: UIViewController {
     
     let dateFormatter = DateFormatter()
     
+    var expenses = [Expense]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         dateFormatter.timeStyle = .long
         dateFormatter.dateStyle = .long
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelagate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelagate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Expense> =  Expense.fetchRequest()
+        
+        do {
+            expenses = try managedContext.fetch(fetchRequest)
+            
+            expensesTableView.reloadData()
+        } catch {
+            print("Fetch could not be performed")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func addNewExpense(_ sender: Any) {
+        performSegue(withIdentifier: "showExpense", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? SingleExpenseViewController,
+            let selectedRow = self.expensesTableView.indexPathForSelectedRow?.row else {
+                return
+        }
         
+        destination.existingExpense = expenses[selectedRow]
+    }
+    
+    func deleteExpense(at indexPath: IndexPath){
+        let expense = expenses[indexPath.row]
+        
+        if let managedContext = expense.managedObjectContext {
+            managedContext.delete(expense)
+            
+            do {
+                try managedContext.save()
+                
+                self.expenses.remove(at: indexPath.row)
+                
+                expensesTableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch {
+                print("Delete failed")
+                
+                expensesTableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+        }
     }
 
 }
 
 extension ExpensesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return expenses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = expensesTableView.dequeueReusableCell(withIdentifier: "expenseCell", for: indexPath)
+        let expense = expenses[indexPath.row]
+        
+        cell.textLabel?.text = expense.name
+        
+        if let date = expense.date {
+            cell.detailTextLabel?.text = dateFormatter.string(from: date)
+        }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            deleteExpense(at: indexPath)
+        }
     }
 }
 
